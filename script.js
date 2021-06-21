3c2a66ac4d112eda7b7633af8aa8b6ee6ca5c4f4
likhithaviswam <76488952+likhithaviswam@users.noreply.github.com>
2021-06-21 19:43:02 +0530

    js removed (#2)

--- script.js
+++
@@ -1,9 +0,0 @@
-function handleLeftNav() {
-    const sub_1 = document.getElementById('sub_1');
-    
-    if(leftNav.classList.contains('left-nav-hidden')) {
-      leftNav.classList.remove('left-nav-hidden');
-    } else {
-      leftNav.classList.add('left-nav-hidden'); 
-    }
-  }
